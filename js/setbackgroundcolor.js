bc8765b221a3bb0a472c0bde3a2395a93a9612b2
ERLMKea <erlm@kea.dk>
2022-09-07 10:29:32 +0200

    try get hex number from screen

--- js/setbackgroundcolor.js
+++ js/setbackgroundcolor.js
@@ -3,6 +3,7 @@ out('Im in setbackground color xx')
 
 const inpColor = document.querySelector(".getColor")
 const pbSetColor = document.querySelector(".pbSetColor")
+const pbSetColorNum = document.querySelector(".pbSetColorNum")
 
 out(inpColor)
 
@@ -15,4 +16,18 @@ function changeBodyColor() {
   bdy.style.backgroundColor = col
 }
 
+function changeBodyColorNum() {
+  let col = inpColor.value;
+  const num = Number(col)
+  const numhx = parseInt(col, 16)
+  out(num)
+  out(numhx)
+  const hexcol = new Number(numhx).toString(16)
+  out(hexcol)
+  bdy.style.backgroundColor = "#" + hexcol
+}
+
+
 pbSetColor.addEventListener('click', changeBodyColor)
+pbSetColorNum.addEventListener('click', changeBodyColorNum)
+
