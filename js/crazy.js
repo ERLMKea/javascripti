bfbaca44d010006d9bc0fb42a3a170b79aa76ef5
ERLMKea <erlm@kea.dk>
2022-09-07 09:04:44 +0200

    Initial commit

---
+++ js/crazy.js
@@ -0,0 +1,34 @@
+console.log("I am in crazy")
+
+function out(any) {console.log(any)}
+function sum(x,y) { return x+y }
+
+const a = 5
+console.log(a)
+let b = 6
+console.log(b)
+b = "hello"
+console.log(b)
+
+out(sum(5,6))
+out(sum(5, "hello"))
+out(sum(4>3, "hello"))
+out(sum(3>4, "hello"))
+out(sum(5.5, "hello"))
+
+
+const c = 0/0;
+let d = c + 5;
+let e = null
+let f
+
+out(e);
+
+out(sum(c, "hellox"))
+out(f)
+out(f, c)
+
+
+
+
+
