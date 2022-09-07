35c2f36874a74cb6a83c628613fcb8761291bc1a
ERLMKea <erlm@kea.dk>
2022-09-07 12:32:37 +0200

    more types

--- js/types.js
+++ js/types.js
@@ -10,11 +10,14 @@ function showTypes(lst) {
 let arr = [1,2]
 out(arr)
 
-arr = [1, 5, "hello", 5.5 , 4>3, [1,2], {"firstName" : "Erik"}, null]
+arr = [1, 5, "hello", 5.5 , 4>3, [1,2], {"firstName" : "Erik"}, null, 'a']
 out(arr)
 showTypes(arr)
 out("hello there")
 
 let aa
 arr.push(aa)
+showTypes(arr)
+let n = 0/0
+arr.push(n)
 showTypes(arr)
