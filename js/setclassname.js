747fc191901121f9b65ed56c885fba7b0a3f113f
ERLMKea <erlm@kea.dk>
2022-09-08 10:35:55 +0200

    set classlist

--- js/setclassname.js
+++ js/setclassname.js
@@ -5,11 +5,18 @@ const inpStyleName = document.getElementById("styleName")
 const pbSetStyle = document.getElementById("pbSetStyle")
 
 function setStyle() {
-  let elm = inpElementName.value;
-  out(elm);
-  let style = inpStyleName.value;
-  out(style)
-  elm.className = style;
+  let elmStr = inpElementName.value;
+  out(elmStr);
+  elm = document.querySelector("." + elmStr)
+  let styleName = inpStyleName.value;
+  out(styleName)
+  out(elm.classList)
+
+  elm.className = '';
+  elm.classList.add(styleName);
+  elm.classList.add(inpElementName.value); //this should add the element name to the classlist
+  out(elm.classList)
+
 }
 
 pbSetStyle.addEventListener('click', setStyle)
