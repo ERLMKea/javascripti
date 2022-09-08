c83e2a4e54cb2557745b52eb5305c6ad93a2471e
ERLMKea <erlm@kea.dk>
2022-09-08 08:56:49 +0200

    more css

--- js/crazy.js
+++ js/crazy.js
@@ -2,6 +2,7 @@ console.log("I am in crazy")
 
 function out(any) {console.log(any)}
 function sum(x,y) { return x+y }
+function sub(x,y) { return x-y }
 
 const a = 5
 console.log(a)
@@ -10,25 +11,17 @@ console.log(b)
 b = "hello"
 console.log(b)
 
-out(sum(5,6))
-out(sum(5, "hello"))
-out(sum(4>3, "hello"))
-out(sum(3>4, "hello"))
-out(sum(5.5, "hello"))
+//out(sum(5,6))
+//out(sum(5, "hello"))
+//out(sum(4>3, "hello"))
+//out(sum(3>4, "hello"))
+//out(sum(5.5, "hello"))
 
 
-const c = 0/0;
-let d = c + 5;
-let e = null
-let f
+out(sub(5, 4>31))
 
-out(e);
-
-out(sum(c, "hellox"))
-out(f)
-out(f, c)
-
-
-
-
+let bb
+if (bb) {
+  out("hej")
+}
 
